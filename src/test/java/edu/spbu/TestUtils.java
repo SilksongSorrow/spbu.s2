11b39f5a5b29d1abd8a8cc36a968b299eda066fb
Last Stade <K.Alexander2003.again@gmail.com>
2022-09-09 18:06:59 +0300

    version 0.1.1 editing tests

---
+++ src/test/java/edu/spbu/TestUtils.java
@@ -0,0 +1,30 @@
+//Uzery Game Studio 2019-2022
+//documentation = false
+
+package edu.spbu;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class TestUtils{
+    /**
+     * @param size array size
+     * @param seed for the pseudo random generator
+     * @return random generated int array. It will be the same for the same seed and size.
+     */
+    public static int[] generateRandomIntArray(int size,long seed){
+        int[] array=new int[size];
+        Random rnd=new Random(seed);
+        for(int i=0;i<array.length;i++){
+            array[i]=rnd.nextInt();
+        }
+        return array;
+    }
+    public static long testN(Supplier<Long> s,int size){
+        long estimatedTime=0;
+        for(int i=0;i<size;i++){
+            estimatedTime+=s.get();
+        }
+        return estimatedTime/size;
+    }
+}
