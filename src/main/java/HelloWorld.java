eb1ef70b2a0ed5138823325b98c67895e244038b
Last Stade <K.Alexander2003.again@gmail.com>
2022-09-09 16:28:23 +0300

    add simple program in HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Пример программы на Java
  */
 // название главного класса, обычно совпадает с названием программы
 public class HelloWorld{
-    public static void main(String[] args){
-        String helloMessage="Hello World!";
-        System.out.println(helloMessage);
+    public static void main(String[] args) throws IOException{
+        System.out.println("Start process");
+        BufferedReader rd=new BufferedReader(new InputStreamReader(System.in));
+        String s=rd.readLine();
+        System.out.println("reverse: "+reverse(s));
+        System.out.println("End process");
+    }
+    private static String reverse(String in){
+        StringBuilder b=new StringBuilder();
+        for(int i=in.length()-1;i>=0;i--){
+            b.append(in.charAt(i));
+        }
+        return b.toString();
     }
 }
